81feb1ce52979a2ac0049b0535ac1c1ed67cca89
ParkHaeSeong <cord7894@gmail.com>
2019-08-05 15:17:12 +0900

    Initial Commit

---
+++ NetStatus/NetStatus.h
@@ -0,0 +1,19 @@
+//
+//  NetStatus.h
+//  NetStatus
+//
+//  Created by rhino Q on 05/08/2019.
+//  Copyright © 2019 rhino Q. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NetStatus.
+FOUNDATION_EXPORT double NetStatusVersionNumber;
+
+//! Project version string for NetStatus.
+FOUNDATION_EXPORT const unsigned char NetStatusVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NetStatus/PublicHeader.h>
+
+
